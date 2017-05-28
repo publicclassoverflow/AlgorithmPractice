507b5672a6a9a59b426dce9f8c2e992f7237d1c2
meng <mz.zhoumeng@gmail.com>
2017-05-27 23:47:26 -0700

    Finished Binary Tree Traversals. Need to figure out non-recursive (stack) method of postorder traversal.

--- src/C/BinaryTreeNDivideAndConquer/Easy/BinaryTreePostOrderTraversal/BinaryTreePostorderTraversal.java
+++ src/C/BinaryTreeNDivideAndConquer/Easy/BinaryTreePostOrderTraversal/BinaryTreePostorderTraversal.java
@@ -1,6 +1,7 @@
-package C.BinaryTreeNDivideAndConquer.Easy.BinaryTreePostOrderTraversal;
+package C.BinaryTreeNDivideAndConquer.Easy.BinaryTreePostorderTraversal;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * http://www.lintcode.com/en/problem/binary-tree-postorder-traversal/
@@ -8,21 +9,33 @@ import java.util.ArrayList;
  *
  * Example
  * Given binary tree {1,#,2,3},
+ *      1
+ *      \
+ *      2
+ *     /
+ *    3
  * return [3,2,1].
  */
+
 public class BinaryTreePostorderTraversal {
     public static void main(String[] args) {
+        Solution solution = new Solution();
         // Build a testing binary tree {1, #, 2, 3}
         TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
-        Solution solution = new Solution();
-        System.out.println(solution.postorderTraversal(root));
+        System.out.println(
+                "Expected output: [3, 2, 1]. " +
+                "Actual output: " + solution.postorderTraversal(root)
+        );
         // Build a testing binary tree {1, 2, 3}
         TreeNode node = new TreeNode(1);
         node.left = new TreeNode(2);
         node.right = new TreeNode(3);
-        System.out.println(solution.postorderTraversal(node));
+        System.out.println(
+                "Expected output: [2, 3, 1]. " +
+                "Actual output: " + solution.postorderTraversal(node)
+        );
     }
 }
 
@@ -58,6 +71,7 @@ class Solution {
         if (root == null) {
             return result;
         }
+        // Method #1: Divide and Conquer
         // Divide
         ArrayList<Integer> leftChild = postorderTraversal(root.left);
         ArrayList<Integer> rightChild = postorderTraversal(root.right);
@@ -66,5 +80,7 @@ class Solution {
         result.addAll(rightChild);
         result.add(root.val);
         return result;
+
+        // Method #2: System stack
     }
 }
